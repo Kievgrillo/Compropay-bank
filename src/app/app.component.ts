ba2f217dfa0ffd11a4803d50f106702746750b8a
keven souza grillo <kevengrillo@gmail.com>
2022-04-21 23:49:18 -0300

    seção 3: acrescentando histórico, mensagens de erros e data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { TransferenciaService } from './services/transferencia.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,10 +9,13 @@ import { Component } from '@angular/core';
 
   export class AppComponent {
     title = 'bytebank';
-    transferencia: any;
+    transferencias: any[] = [];
+
+    constructor(private service: TransferenciaService){
+
+    }
 
     transferir($event) {
-      console.log($event);
-      this.transferencia = $event;
+      this.service.adicionar($event);
     }
   }
